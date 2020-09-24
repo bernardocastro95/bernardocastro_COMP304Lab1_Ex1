26830c1ee2ee6660c563b4d506df44b9b58f940b
Bernardo Castro <bernardoc544@gmail.com>
2020-09-24 12:23:30 -0400

    Toast

--- app/src/main/java/com/example/bernardocastro_comp304lab1_ex1/ActivitiesTypes.java
+++ app/src/main/java/com/example/bernardocastro_comp304lab1_ex1/ActivitiesTypes.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -27,8 +29,18 @@ public class ActivitiesTypes extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_activities_types, container, false);
         ListView lv = view.findViewById(R.id.types);
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, types);
+        final ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, types);
         lv.setAdapter(adapter);
+        
+        
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(getActivity(), "Testing", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        
         return view;
     }
 }
