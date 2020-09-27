55a79c836616e5ff9d25e6b9c8b59270f02f3562
Bernardo Castro <bernardoc544@gmail.com>
2020-09-27 14:43:35 -0400

    Final adjustments on method

--- app/src/main/java/com/example/bernardocastro_comp304lab1_ex1/ActivityCycle.java
+++ app/src/main/java/com/example/bernardocastro_comp304lab1_ex1/ActivityCycle.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 
 public class ActivityCycle extends Fragment {
 
-    TextView type;
+    TextView type, start, create;
 
     public ActivityCycle() {
 
@@ -21,17 +21,22 @@ public class ActivityCycle extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_activity_cycle, container, false);
-        TextView start = view.findViewById(R.id.onStart);
-        TextView create = view.findViewById(R.id.onCreate);
+        start = view.findViewById(R.id.onStart);
+        create = view.findViewById(R.id.onCreate);
         type = view.findViewById(R.id.activity_type);
         String StartTest = getResources().getString(R.string.start);
         start.setText(StartTest);
+        start.setVisibility(View.GONE);
         String CreateText = getResources().getString(R.string.create);
         create.setText(CreateText);
+        create.setVisibility(View.GONE);
         return view;
     }
 
     public void show(String activity){
         type.setText(activity);
+        start.setVisibility(View.VISIBLE);
+        create.setVisibility(View.VISIBLE);
+
     }
 }
